84ba54d168e5a4e4b83ea48f09119ba05a11d427
Ansh <anshgagneja1614@gmail.com>
2025-02-07 14:01:25 +0530

    Fixed headers already sent issue in login-body.php and header.php

--- inc/login-body.php
+++ inc/login-body.php
@@ -65,3 +65,74 @@ if (isset($_POST['submit'])) {
 }
 ob_end_flush();
 ?>
+<body>
+    <!-- Admin Panel HTML codes will be written here(Starts)-->
+
+    <div class="container-fluid">
+      <div class="row" style="padding-top: 10%">
+            <div class="col-md-4"></div>
+            <div class="col-md-4" style="text-align: center;
+            background-color: white; border-radius: 20px;
+            padding:10px;">
+              <div class="row">
+                <div class="col-md-12"  style="padding-bottom: 15px;">
+                  <img src="https://static.vecteezy.com/system/resources/thumbnails/011/401/535/small/online-shopping-trolley-click-and-collect-order-logo-design-template-vector.jpg" height="70">
+                </div>
+
+                <div class="col-md-12">
+                  <span style=" font-weight: 100; font-size: 18px">
+                    Login to your account</span>
+                </div>
+                <div class="col-md-12">
+                  <form method="POST">
+                    <div class="row">
+                      <div class="col-md-12" style="text-align: left; font-size: 14px; font-weight: 200; padding: 20px 20px 10px 20px">
+                        <label>Your Email</label>
+                        <input type="email" name="useremail" placeholder="Username"
+                        class="form-control">
+                      </div>
+                      <div class="col-md-12" style="text-align: left; font-size: 14px; font-weight: 200; padding: 10px 20px 10px 20px">
+                        <label>Your Password</label>
+                        <input type="password" name="password" placeholder="Password"
+                        class="form-control">
+                      </div>
+
+
+                      <div class="col-md-12" style="text-align: left; font-size: 14px; font-weight: 200; padding: 10px 20px 10px 20px">
+                        <label>Role</label>
+                        <select name="role">
+                            <option value="Customer">Customer</option>
+                            <option value="Admin">Admin</option>
+                        </select>
+                        
+                      </div>
+
+
+
+                      <div class="col-md-12" style="text-align: center; font-size: 14px; font-weight: 200; padding: 10px 20px 10px 20px">
+                       <!--<a href="index.php?page=Authenticate" class="btn btn-warning">Login now</a> -->
+                       <button name="submit" class="btn btn-warning">Login Now</button>
+                      </div>
+
+                      <div class="col-md-12" style="text-align: center; font-size: 14px; font-weight: 200; padding: 0px 20px 10px 20px">
+                       <div class="row">
+                         <div class="col-md-6" style="text-align: left;
+                         font-size: 12px;
+                         font-weight:100 ">
+                         <a href="index.php?page=Register" style="text-decoration: none; color: black;">
+                           No Account? Register Now </a> 
+
+                         </div>
+                         
+                       </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4"></div>
+      </div>
+    </div>
+
+    <!-- Admin Panel HTML codes will be written here (End)-->
